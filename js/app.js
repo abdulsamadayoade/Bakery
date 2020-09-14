0236dc5f6ce6eff99e02c9875c8744a281a82552
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-09-14 23:13:22 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('show');
+});
